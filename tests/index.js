ccaebc64326bdb2fbec711216241bb2341b7acfc
mauricebutler <maurice.butler@gmail.com>
2015-09-07 17:05:11 +1000

    tests and readme etc

--- tests/index.js
+++ tests/index.js
@@ -1,2 +1,31 @@
-console.log(require('./someDir/someFile'));
-console.log(require('./someDir/deeperDir'));
+var test = require('tape');
+
+test('file', function(t) {
+    t.plan(1);
+
+    t.equal(
+        require('./someDir/someFile'),
+        'majigger',
+        'Resolves correctly'
+    );
+});
+
+test('directory', function(t) {
+    t.plan(1);
+
+    t.equal(
+        require('./someDir/deeperDir'),
+        'majigger',
+        'Resolves correctly'
+    );
+});
+
+test('legit modules', function(t) {
+    t.plan(1);
+
+    t.equal(
+        require('tape'),
+        test,
+        'Resolves correctly'
+    );
+});
